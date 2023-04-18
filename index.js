c2979a46831abd4647f3087d19d981095756cfa0
Mick <tr1al3xp@gmail.com>
2023-04-18 15:24:15 +0300

    index cleaning

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 const fs = require('node:fs');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const JsonOperations = require('./db/JSONOperations.js');
-const CacheProvider = require('./app/cacheProvider/cacheProvider.js');
-const UserService = require('./app/users/userService.js');
-const UsersController = require('./cocktroller.js');
 const routes = require('./cocktroller.js');
 
-const router = express.Router();
-const CreateUserInteractor = require('./app/users/createUser/createUserInteractor.js');
-const DeleteUserInteractor = require('./app/users/deleteUser/deleteUserInteractor.js');
-const GetUsers = require('./app/users/getUsers/getUsersInteractor.js');
-const UpdateUserInteractor = require('./app/users/updateUser/updateUserInteractor.js');
-
 (async () => {
-    /* const JSON_PATH = './db/db.json';
-    
-    const jsonOperations = new JsonOperations({ dbPath: JSON_PATH }, { fs });
-    const cacheProvider = new CacheProvider();
-    
-    const userService = new UserService(jsonOperations, cacheProvider); */
-
-    const jsonBodyParser = bodyParser.json();
 
     app.use('/', routes);
 
